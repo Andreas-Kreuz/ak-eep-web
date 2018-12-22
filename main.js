065059777e69e6ab7ed92278599809a8047ce071
Andreas Kreuz <andreas.kreuz@endlich-zeit-haben.de>
2018-12-22 17:29:35 +0100

    Autoformat main.js

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
-const { app, BrowserWindow } = require('electron')
+const {app, BrowserWindow} = require('electron');
 
 let win;
 
-function createWindow () {
+function createWindow() {
   // Create the browser window.
   win = new BrowserWindow({
     width: 600,
     height: 600,
     backgroundColor: '#ffffff',
     icon: `file://${__dirname}/dist/assets/logo.png`
-  })
+  });
 
 
-  win.loadURL(`file://${__dirname}/dist/ak-eep-web/index.html`)
+  win.loadURL(`file://${__dirname}/dist/ak-eep-web/index.html`);
 
   //// uncomment below to open the DevTools.
   // win.webContents.openDevTools()
@@ -24,7 +24,7 @@ function createWindow () {
 }
 
 // Create window on electron intialization
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
@@ -33,11 +33,11 @@ app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') {
     app.quit()
   }
-})
+});
 
 app.on('activate', function () {
   // macOS specific close process
   if (win === null) {
     createWindow()
   }
-})
+});
